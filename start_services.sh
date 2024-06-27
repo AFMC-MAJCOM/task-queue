2690226a426ce45ecdc16aa6d57aa2acfc076cab
JR-USAF <justin.roberts.27@us.af.mil>
2024-06-27 23:06:15 +0000

    working bash script

--- start_services.sh
+++ start_services.sh
@@ -1,9 +1,9 @@
-if [ $1 == "controller" ]
-then
-python ./data_pipeline/work_queue_service_cli.py "${@:2}"
-elif [ $1 == "server"]
-then
-python ./data_pipeline/work_queue_web_api.py "${@:2}"
+if [ $1 == "controller" ]; then
+    echo "Runner controller cli with args ${@:2}"
+    python ./data_pipeline/work_queue_service_cli.py "${@:2}"
+elif [ $1 == "server" ]; then
+    echo "Running web api server with args: ${@:2}"
+    python ./data_pipeline/work_queue_web_api.py "${@:2}"
 else
-echo "First argument must be `controller` or `server`"
+    echo "First argument must be 'controller' or 'server'"
 fi
