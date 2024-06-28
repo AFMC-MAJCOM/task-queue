5190fe07bc1ae72a83fc97db5ded58f75cd18613
Justin Roberts <justin.roberts.27@us.af.mil>
2024-06-28 16:59:53 -0600

    update start services with uvicorn

--- start_services.sh
+++ start_services.sh
@@ -4,8 +4,8 @@ if [ $1 == "controller" ]; then
     echo "Runner controller cli with args ${@:2}"
     python work_queue_service_cli.py "${@:2}"
 elif [ $1 == "server" ]; then
-    echo "Running web api server with args: ${@:2}"
-    python work_queue_web_api.py "${@:2}"
+    echo "Running web api server"
+    uvicorn work_queue_web_api:app --reload --port=80 --host 0.0.0.0
 else
     echo "First argument must be 'controller' or 'server'"
 fi
