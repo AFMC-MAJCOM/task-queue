5742bacd45595c05edc4d6cb65b1de558d889858
AM-USAF <anna.mendenhall.1@us.af.mil>
2024-07-11 20:43:35 +0000

    fixed version check setup

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup
 
+def read(rel_path: str) -> str:
+    here = os.path.abspath(os.path.dirname(__file__))
+    # intentionally *not* adding an encoding option to open.
+    # See: https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    with open(os.path.join(here, rel_path)) as file_path:
+        return file_path.read()
+
 def get_version(rel_path: str) -> str:
     for line in read(rel_path).splitlines():
         if line.startswith("__version__"):
