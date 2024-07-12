b5e4908fe2d1658514702640197645f7e9366e2f
Justin Roberts <justin.roberts.27@us.af.mil>
2024-07-12 16:23:58 -0600

    pip freeze did not work, reverting back

--- start_services.sh
+++ start_services.sh
@@ -7,9 +7,7 @@ elif [ $1 == "server" ]; then
     echo "Running web api server"
     uvicorn work_queue_web_api:app --reload --port=80 --host 0.0.0.0
 elif [ $1 == "version" ]; then
-    full_version=$(pip freeze | grep data-pipeline)
-    version=${full_version##*==}
-    echo $version 
+    python -c "import data_pipeline; print(data_pipeline.__version__)"
 else
     echo "First argument must be 'controller', 'server', or 'version'"
 fi
