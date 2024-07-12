06adf64b4c33888f3895fe63d377edabad21e89b
Justin Roberts <justin.roberts.27@us.af.mil>
2024-07-12 16:11:36 -0600

    use pip freeze

--- start_services.sh
+++ start_services.sh
@@ -7,7 +7,9 @@ elif [ $1 == "server" ]; then
     echo "Running web api server"
     uvicorn work_queue_web_api:app --reload --port=80 --host 0.0.0.0
 elif [ $1 == "version" ]; then
-    python3 -c "import data_pipeline; print(data_pipeline.__version__)"
+    full_version=$(pip freeze | grep data-ipeline)
+    version=${full_version##*==}
+    echo $version 
 else
     echo "First argument must be 'controller', 'server', or 'version'"
 fi
