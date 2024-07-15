44fb11a3b16bd0e4e3cb5560fe719936ca364c27
AM-USAF <anna.mendenhall.1@us.af.mil>
2024-07-15 12:38:08 -0600

    OPAL-603 Acceptance Demo Fixes (#18)
    * Fixed incorrect line formatting
    
    * moving conftest bucket creation and cleanup to file specific
    
    ---------
    
    Co-authored-by: AB-USAF <alexander.bedoya@us.af.mil>

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,30 +5,6 @@
 import os
 import pytest
 
-import s3fs
-
-
-@pytest.fixture(scope="session", autouse=True)
-def setup_s3_bucket():
-    """Create a 'unit-tests' S3 bucket for testing purposes.
-    """
-    fs = s3fs.S3FileSystem()
-
-    test_bucket_name = 'unit-tests'
-    if fs.exists(test_bucket_name):
-        fs.rm(test_bucket_name, recursive=True)
-    fs.mkdir(test_bucket_name)
-
-    yield
-    cleanup_bucket(test_bucket_name, fs)
-
-
-def cleanup_bucket(test_bucket_name, fs):
-    """Delete the created bucket that was used for testing.
-    """
-    if fs.exists(test_bucket_name):
-        fs.rm(test_bucket_name)
-
 
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_makereport(item, call):
