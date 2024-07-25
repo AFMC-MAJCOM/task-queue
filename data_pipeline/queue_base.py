68978e0ddc29d0f3a9dedd74d044500b0308ec67
AM-USAF <anna.mendenhall.1@us.af.mil>
2024-07-25 22:14:46 +0000

    rework QueueBase as ABC

--- data_pipeline/queue_base.py
+++ data_pipeline/queue_base.py
@@ -1,8 +1,7 @@
-"""Wherein is contained the Base Classes for Queue.
+"""Wherein is contained the Abstract Base Classes for Queue.
 """
+from abc import ABC, abstractmethod
 from enum import Enum
-from dataclasses import dataclass
-from typing import Callable, Any, List, Tuple, Dict, Optional
 
 
 class QueueItemStage(Enum):
@@ -13,14 +12,94 @@ class QueueItemStage(Enum):
     SUCCESS = 2
     FAIL = 3
 
-@dataclass
-class QueueBase():
-    """Base Class for Queue.
+
+class QueueBase(ABC):
+    """Abstract Base Class for Queue.
     """
-    put : Callable[[Dict[str, Any]], None]
-    get : Callable[[Optional[int]], List[Tuple[str, Any]]]
-    success : Callable[[str], Any]
-    fail : Callable[[str], Any]
-    size : Callable[[QueueItemStage], int]
-    lookup_status : Callable[[str], QueueItemStage]
-    description : Dict[str, str]
+    @abstractmethod
+    def __init__(self):
+        """Initializes the QueueBase class.
+        """
+
+    @abstractmethod
+    def put(self, items):
+        """Adds a new Item to the Queue in the WAITING stage.
+
+        Parameters:
+        -----------
+        items: dict
+            Dictionary of Queue Items to add Queue, where Item is a key:value
+            pair, where key is the item ID and value is the queue item body.
+        """
+
+    @abstractmethod
+    def get(self, n_items=1):
+        """Gets the next n Items from the Queue, moving them to PROCESSING.
+
+        Parameters:
+        -----------
+        n_items: int (default=1)
+            Number of items to retrieve from Queue.
+
+        Returns:
+        ------------
+        Returns a list of n_items from the Queue, as
+        List[(queue_item_id, queue_item_body)]
+        """
+
+    @abstractmethod
+    def success(self, queue_item_id):
+        """Moves a Queue Item from PROCESSING to SUCCESS.
+
+        Parameters:
+        -----------
+        queue_item_id: str
+            ID of Queue Item
+        """
+
+    @abstractmethod
+    def fail(self, queue_item_id):
+        """Moves a Queue Item from PROCESSING to FAIL.
+
+        Parameters:
+        -----------
+        queue_item_id: str
+            ID of Queue Item
+        """
+
+    @abstractmethod
+    def size(self, queue_item_stage):
+        """Determines how many Items are in some stage of the Queue.
+
+        Parameters:
+        -----------
+        queue_item_stage: QueueItemStage object
+            The specific stage of the Queue (PROCESSING, FAIL, etc.).
+
+        Returns:
+        ------------
+        Returns the number of Items in that stage of the Queue as an integer.
+        """
+
+    @abstractmethod
+    def lookup_status(self, queue_item_id):
+        """Lookuup which stage in the Queue Item is currently in.
+
+        Parameters:
+        -----------
+        queue_item_id: str
+            ID of Queue Item
+
+        Returns:
+        ------------
+        Returns the current stage of the Item as a QueueItemStage object.
+        """
+
+    @abstractmethod
+    def description():
+        """A brief description of the Queue.
+
+        Returns:
+        ------------
+        Returns a dictionary with relevant information about the Queue.
+        """
--- data_pipeline/queue_with_events.py
+++ data_pipeline/queue_with_events.py
@@ -1,7 +1,6 @@
 """Contains functions and classes concerning Queue with Event.
 """
 from typing import Dict
-from functools import partial
 
 import pydantic
 
@@ -63,107 +62,187 @@ class QueueMoveEventData(pydantic.BaseModel):
         """
         return v.value
 
-# Pylint disabled because BaseException is used to set exc
-# pylint: disable=broad-exception-caught
-def add_to_queue_with_event(
-    queue,
-    event_store,
-    queue_add_event_name,
-    new_items
-):
-    """Adds Item to the Queue and logs the Event to an Event Store.
-
-    Parameters:
-    -----------
-    queue: QueueBase
-        Queue to add Items to.
-    event_store: EventStoreInterface
-        Event Store to add Events to.
-    queue_add_event_name: str
-        Name of Event.
-    new_items: Dict
-        New Items to add to queue.
-    Returns:
-    -----------
-    Results of putting filtered_items in queue in the WAITING stage.
+class QueueWithEvents(QueueBase):
+    """Class for QueueWithEvents.
     """
-    # The flow control of this function looks really weird to satisfy the
-    # `test_put_with_exception` test.
-    queue_event_data = []
-    filtered_items = {}
-    exc = None
+    def __init__(
+        self,
+        queue,
+        event_store,
+        event_base_name,
+        add_event_name,
+        move_event_name
+    ):
+        """Initializes the QueueWithEvents class.
+        """
+        self.queue = queue
+        self.event_store = event_store
+        self.event_base_name = event_base_name
+        self.add_event_name = add_event_name
+        self.move_event_name = move_event_name
 
-    for k, v in new_items.items():
-        try:
-            queue_event_data.append(
-                Event(
-                    name=queue_add_event_name,
-                    version=QUEUE_EVENT_SCHEMA_VERSION,
-                    data=QueueAddEventData(
-                        queue_index_key=k,
-                        queue_item_data=v,
-                        queue_info=queue.description
-                    ).model_dump()
+    # Pylint disabled because BaseException is used to set exc
+    # pylint: disable=broad-exception-caught
+    def put(self, items):
+        """Adds a new Item to the Queue in the WAITING stage and logs the
+        Event to an Event Store.
+
+        Parameters:
+        -----------
+        items: dict
+            Dictionary of Queue Items to add Queue, where Item is a key:value
+            pair, where key is the item ID and value is the queue item body.
+
+        Returns:
+        -----------
+        Results of putting filtered_items in queue in the WAITING stage.
+        """
+        # The flow control of this function looks really weird to satisfy the
+        # `test_put_with_exception` test.
+        queue_event_data = []
+        filtered_items = {}
+        exc = None
+
+        for k, v in items.items():
+            try:
+                queue_event_data.append(
+                    Event(
+                        name=self.add_event_name,
+                        version=QUEUE_EVENT_SCHEMA_VERSION,
+                        data=QueueAddEventData(
+                            queue_index_key=k,
+                            queue_item_data=v,
+                            queue_info=self.queue.description()
+                        ).model_dump()
+                    )
                 )
+
+                filtered_items[k] = v
+            except BaseException as e:
+                exc = e
+
+        self.event_store.add(queue_event_data)
+
+        out = self.queue.put(filtered_items)
+
+        if exc is not None:
+            raise exc
+
+        return out
+
+    def get(self, n_items=1):
+        """Gets the next n items from the queue, moving them to PROCESSING and
+        logs the Event.
+
+        Parameters:
+        -----------
+        n_items: int (default=1)
+            Number of items to retrieve from queue.
+
+        Returns:
+        ------------
+        Returns a list of n_items from the queue, as
+        List[(queue_item_id, queue_item_body)]
+        """
+        n_items = max(n_items, 0)
+
+        items = self.queue.get(n_items)
+
+        queue_event_data = [
+            Event(
+                name=self.move_event_name,
+                version=QUEUE_EVENT_SCHEMA_VERSION,
+                data=QueueMoveEventData(
+                    queue_index_key=k,
+                    stage_from=QueueItemStage.WAITING,
+                    stage_to=QueueItemStage.PROCESSING
+                ).model_dump()
             )
 
-            filtered_items[k] = v
-        except BaseException as e:
-            exc = e
+            for k, _ in items
+        ]
 
-    event_store.add(queue_event_data)
+        self.event_store.add(queue_event_data)
 
-    out = queue.put(filtered_items)
+        return items
 
-    if exc is not None:
-        raise exc
+    def success(self, queue_item_id):
+        """Moves a Queue Item from PROCESSING to SUCCESS and logs the Event.
 
-    return out
-
-
-def get_from_queue_with_event(
-    queue,
-    event_store,
-    queue_move_event_name,
-    n_items = 1
-):
-    """Retrieves Items in WAITING stage, logs Event, then adds to the Store.
-
-    Parameters:
-    -----------
-    queue: QueueBase
-        Queue to get Items from.
-    event_store: EventStoreInterface
-        Event Store to add Events to.
-    queue_move_event_name: str
-        Name of Event.
-    n_items: int (default=1)
-        Number of Items to get from Queue.
-    Returns:
-    -----------
-    List of Items retrieved from the Queue and moved to PROCESSING stage.
-    """
-    n_items = max(n_items, 0)
-
-    items = queue.get(n_items)
-
-    queue_event_data = [
-        Event(
-            name=queue_move_event_name,
-            version=QUEUE_EVENT_SCHEMA_VERSION,
-            data=QueueMoveEventData(
-                queue_index_key=k,
-                stage_from=QueueItemStage.WAITING,
-                stage_to=QueueItemStage.PROCESSING
-            ).model_dump()
+        Parameters:
+        -----------
+        queue_item_id: str
+            ID of Queue Item
+        """
+        record_queue_move_event(
+            self.event_store,
+            self.move_event_name,
+            queue_item_id,
+            QueueItemStage.PROCESSING,
+            QueueItemStage.SUCCESS
         )
 
-        for k, _ in items
-    ]
+        self.queue.success(queue_item_id)
 
-    event_store.add(queue_event_data)
+    def fail(self, queue_item_id):
+        """Moves a Queue Item from PROCESSING to FAIL and logs the Event.
 
-    return items
+        Parameters:
+        -----------
+        queue_item_id: str
+            ID of Queue Item
+        """
+        record_queue_move_event(
+            self.event_store,
+            self.move_event_name,
+            queue_item_id,
+            QueueItemStage.PROCESSING,
+            QueueItemStage.FAIL
+        )
+
+        self.queue.fail(queue_item_id)
+
+    def size(self, queue_item_stage):
+        """Determines how many items are in some stage of the queue.
+
+        Parameters:
+        -----------
+        queue_item_stage: QueueItemStage object
+            The specific stage of the queue (PROCESSING, FAIL, etc.).
+
+        Returns:
+        ------------
+        Returns the number of items in that stage of the queue as an integer.
+        """
+        return self.queue.size(queue_item_stage)
+
+    def lookup_status(self, queue_item_id):
+        """Lookup which stage in the Queue Item is currently in.
+
+        Parameters:
+        -----------
+        queue_item_id: str
+            ID of Queue Item
+
+        Returns:
+        ------------
+        Returns the current stage of the Item as a QueueItemStage object.
+        """
+        return self.queue.lookup_status(queue_item_id)
+
+    def description(self):
+        """A brief description of the Queue.
+
+        Returns:
+        ------------
+        Returns a dictionary with relevant information about the Queue.
+        """
+        desc = {
+            "implementation": "event",
+            "event_base_name": self.event_base_name,
+            "base_queue_description": self.queue.description()
+        }
+        return desc
 
 
 def record_queue_move_event(
@@ -200,66 +279,6 @@ def record_queue_move_event(
 
     event_store.add(queue_event_data)
 
-
-def queue_success_with_event(
-    queue,
-    event_store,
-    queue_move_event_name,
-    item_id
-):
-    """Moves Queue Item from PROCESSING to SUCCESS and logs the Event.
-
-    Parameters:
-    -----------
-    queue: QueueBase
-        Queue to add Event to SUCCESS stage.
-    event_store: EventStoreInterface
-        Event Store to track item movement.
-    queue_move_event_name: str
-        Name of Event.
-    item_id: str
-        ID of Queue Item.
-    """
-    record_queue_move_event(
-        event_store,
-        queue_move_event_name,
-        item_id,
-        QueueItemStage.PROCESSING,
-        QueueItemStage.SUCCESS
-    )
-
-    queue.success(item_id)
-
-def queue_fail_with_event(
-    queue,
-    event_store,
-    queue_move_event_name,
-    item_id
-):
-    """Moves Queue Item from PROCESSING to FAIL and logs the Event.
-
-    Parameters:
-    -----------
-    queue: QueueBase
-        Queue to add Event to Fail stage.
-    event_store: EventStoreInterface
-        Event Store to track item movement.
-    queue_move_event_name: str
-        Name of Event.
-    item_id: str
-        ID of Queue Item.
-    """
-    record_queue_move_event(
-        event_store,
-        queue_move_event_name,
-        item_id,
-        QueueItemStage.PROCESSING,
-        QueueItemStage.FAIL
-    )
-
-    queue.fail(item_id)
-
-
 def queue_with_events(
     queue,
     event_store,
@@ -267,24 +286,7 @@ def queue_with_events(
     add_event_name = None,
     move_event_name = None
 ):
-    """Returns a QueueBase object with added functionality to log events as
-    queue items are retrieved and moved.
-
-    Parameters:
-    -----------
-    queue: QueueBase
-    event_store: EventStoreInterface
-        Event Store to log item movement.
-    event_base_name: str (default=None)
-        Name of Base Event.
-    add_event_name: str (default=None)
-        Add Event name.
-    move_event_name: str (default=None)
-        Move Event name.
-
-    Returns:
-    -----------
-    QueueBase object.
+    """Creates a QueueWithEvents object.
     """
     if event_base_name:
         add_event_name = f"{event_base_name}_ADD"
@@ -293,19 +295,10 @@ def queue_with_events(
     # validate arguments
     assert bool(add_event_name) and bool(move_event_name), \
         "No event name supplied for queue"
-
-    return QueueBase(
-        partial(add_to_queue_with_event, queue, event_store, add_event_name),
-        partial(get_from_queue_with_event, queue,
-                event_store, move_event_name),
-        partial(queue_success_with_event, queue, event_store, move_event_name),
-        partial(queue_fail_with_event, queue, event_store, move_event_name),
-        queue.size,
-        queue.lookup_status,
-
-        {
-            "implementation": "event",
-            "event_base_name": event_base_name,
-            "base_queue_description": queue.description
-        }
+    return QueueWithEvents(
+        queue,
+        event_store,
+        event_base_name,
+        add_event_name,
+        move_event_name
     )
