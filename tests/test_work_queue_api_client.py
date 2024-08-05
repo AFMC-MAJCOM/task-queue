99c880314f961598b6e79e7bbe2b7833a72f05fe
AM-USAF <anna.mendenhall.1@us.af.mil>
2024-08-05 07:03:09 -0600

    OPAL 643: Client Existing Queue API Functions (#43)
    * api pytests made and passing
    
    * added client tests and wrapped existing endpoints in client

--- tests/test_work_queue_api_client.py
+++ tests/test_work_queue_api_client.py
@@ -1,9 +1,63 @@
-"""Tests for validating the work queue api client
+"""Tests for validating the Work Queue API Client
 """
+import pytest
+import re
+from unittest import mock
+from requests.exceptions import RequestException
 
 from task_queue.work_queue_api_client import ApiClient
+from task_queue.work_queue_web_api import app
+
+url = "http://localhost:8000"
+test_client = ApiClient(url)
+
+# Get list of all possible endpoints
+api_routes = [url + x.path for x in app.routes]
+
+def mocked_requests(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.status_code = status_code
+            self.json_data = json_data
+
+        def json(self):
+            return self.json_data
+
+        def raise_for_status(self):
+            if self.status_code >= 400:
+                raise RequestException("ERROR ", self.status_code)
+
+    route = args[0]
+    # Replace item_id passed into url
+    if 'good-item-id' in route:
+        route = re.sub('good-item-id','{item_id}',route)
+
+    if route in api_routes:
+        return MockResponse({"good":"dictionary"},200)
+
+    return MockResponse("Bad URL", 404)
 
 def test_constructor():
-    client = ApiClient("localhost:8080")
+    assert test_client.api_base_url == f"{url}/api/v1/queue/"
 
-    assert client.api_base_url == "localhost:8080/api/v1/queue/"
+@mock.patch('requests.get', side_effect=mocked_requests)
+def test_bad_url(mock_get):
+    # It does not matter what endpoint is used here, we are just testing that
+    # it is properly throwing an error when testing a bad url.
+    with pytest.raises(RequestException):
+        test_client.lookup_status('bad-url')
+
+@mock.patch('requests.get', side_effect=mocked_requests)
+def test_client_lookup_status(mock_get):
+    response = test_client.lookup_status('good-item-id')
+    assert isinstance(response, dict)
+
+@mock.patch('requests.get', side_effect=mocked_requests)
+def test_client_description(mock_get):
+    response = test_client.description()
+    assert isinstance(response, dict)
+
+@mock.patch('requests.get', side_effect=mocked_requests)
+def test_client_get_queue_sizes(mock_get):
+    response = test_client.get_queue_sizes()
+    assert isinstance(response, dict)
