80c9647ef9d5253b36f726b400cecbab8cd48725
CK-USAF <christopher.kauffman.3@us.af.mil>
2024-08-06 14:17:15 +0000

    fixed ruff error

--- tests/test_api_endpoints.py
+++ tests/test_api_endpoints.py
@@ -98,7 +98,7 @@ def test_v1_queue_lookup_item():
     assert response.status_code == 400
     assert response.json() == {"detail":"bad-item-id not in Queue"}
 
-    def test_put_valid_items():
+def test_put_valid_items():
     client.post("/api/v1/queue/put", json=default_items)
     assert queue.size(QueueItemStage.WAITING) == len(default_items)
 
