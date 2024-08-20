492016ba37b12cd683c1b01a0d009b47a2b1a691
CK-USAF <138030014+CK-USAF@users.noreply.github.com>
2024-08-20 14:44:21 -0600

    Docker Image Release Policy (#61)
    * added new functionality to verify if a new image should be created
    
    * heavy overhaul of the runners to accomodate reusable jobs
    
    * added runs on to validate if a docker image should be created
    
    * only releases from main will create a docker image
    
    * changed if statement in release to check if on main not if not on main

---
+++ .github/workflows/verify_new_image.py
@@ -0,0 +1,32 @@
+import sys
+
+
+def verify_new_image(version: str) -> bool:
+    """Verify that if a new docker image should be
+    published based on the version.
+
+    Parameters
+    ----------
+    version: string
+        The version being pushed to main.
+
+    Outputs
+    ----------
+    new_release: bool
+        True if a new docker image should be released, False otherwise.
+    """
+    version = [int(v) for v in version.split('.')]
+    if version[2] == 0:
+        # If the last version number is 0 it indicates that there was a minor
+        # or a major release
+        return True
+    return False
+
+
+if __name__=="__main__":
+    assert len(sys.argv) == 2
+    version = sys.argv[1]
+    print(f" Version: {version}")
+
+    new_release = verify_new_image(version)
+    sys.exit(new_release)
--- task_queue/__init__.py
+++ task_queue/__init__.py
@@ -10,4 +10,4 @@ __all__ = [
     "config",
 ]
 
-__version__ = "1.5.7"
+__version__ = "1.5.8"
