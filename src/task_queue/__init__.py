e72a1e11917304dd2dfe2dac1a94b781361330d7
Noah Jackowitz <noah.jackowitz@us.af.mil>
2024-08-22 23:09:47 +0000

    add build job

--- src/task_queue/__init__.py
+++ src/task_queue/__init__.py
@@ -4,11 +4,10 @@ from .config.config import get_task_queue_settings
 from .config import config
 try:
     from ._version import __version__, __version_tuple__
-except ModuleNotFoundError
+except ModuleNotFoundError:
     raise ValueError('`task_queue` must be `pip install`ed!') from None
 
 __all__ = [
     "get_task_queue_settings",
     "config",
 ]
-
