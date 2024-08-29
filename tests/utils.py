c5d904216d1d47450bd449ab1eae1ce72aeeffd8
AM-USAF <anna.mendenhall.1@us.af.mil>
2024-08-29 17:33:54 +0000

    attempt to revert changes

--- tests/utils.py
+++ tests/utils.py
@@ -4,32 +4,15 @@ import sqlalchemy as sqla
 
 from task_queue import config
 
+settings = config.TaskQueueSqlSettings()
 
+test_sql_engine = sqla.create_engine(sqla.engine.url.URL(
+            drivername="postgresql",
+            username=settings.SQL_QUEUE_POSTGRES_USER,
+            password=settings.SQL_QUEUE_POSTGRES_PASSWORD,
+            host=settings.SQL_QUEUE_POSTGRES_HOSTNAME,
+            database=settings.SQL_QUEUE_POSTGRES_DATABASE,
+            query={},
+            port=settings.SQL_QUEUE_POSTGRES_PORT,
 
-class PytestSqlEngine():
-    """Class to create a SQL engine used for testing."""
-
-    def __init__():
-        """Initializing PytestSqlEngine object from envrironemnt variables."""
-        settings = config.TaskQueueSqlSettings()
-
-        self.drivername = "postgresql"
-        self.username = settings.SQL_QUEUE_POSTGRES_USER
-        self.password = settings.SQL_QUEUE_POSTGRES_PASSWORD
-        self.host = settings.SQL_QUEUE_POSTGRES_HOSTNAME
-        self.database = settings.SQL_QUEUE_POSTGRES_DATABASE
-        self.query = {}
-        self.port = settings.SQL_QUEUE_POSTGRES_PORT
-
-    def create_test_engine():
-        """Create and return SQL engine to use for testing."""
-        test_sql_engine = sqla.create_engine(sqla.engine.url.URL(
-                    self.drivername,
-                    self.username,
-                    self.password,
-                    self.host,
-                    self.database,
-                    self.query,
-                    self.port,
-                ), echo=True)
-        return test_sql_engine
+        ), echo=True)
