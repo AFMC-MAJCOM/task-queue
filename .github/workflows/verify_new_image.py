f3c58cbc8b96961c7f1b495e584c26bb299e8f56
Noah Jackowitz <noah.jackowitz@us.af.mil>
2024-08-29 09:53:47 -0600

    Publish docker images for all releases, not just major/minor
    Any release not released is not a release.
    That would be a pre-, post-, pr dev-release

--- .github/workflows/verify_new_image.py
+++ .github/workflows/verify_new_image.py
@@ -1,42 +1,31 @@
 #! /usr/bin/env python
-import re
-import sys
+"""Determine if a new docker image should be published for the version."""
+
+import argparse
 import os
+import sys
 
-# Modified version pattern from https://semver.org/. If the 'patch' number
-# is 0 and there is no prerelease or build metadata it indicates that there
-# was a minor or a major release
-RELEASABLE_VERSION_PATTERN = re.compile(r'^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0)$')
+from packaging.version import Version  # 3rd-party, may need to pip install
 
 
-def verify_new_image(version_string: str) -> bool:
-    """Verify that if a new docker image should be
-    published based on the version.
-
-    Parameters
-    ----------
-    version: string
-        The version being pushed to main.
-
-    Outputs
-    ----------
-    new_release: bool
-        True if a new docker image should be released, False otherwise.
-    """
-    return bool(RELEASABLE_VERSION_PATTERN.fullmatch(version_string))
+parser = argparse.ArgumentParser(usage=__doc__)
+parser.add_argument('version', type=Version)
 
 
 if __name__=="__main__":
-    print(sys.argv)
-    assert len(sys.argv) == 2
-    version = sys.argv[1]
-    print(f" Version: {version}")
+    args = parser.parse_args()
+    print(f" Version: {args.version}")
 
-    new_release = verify_new_image(version)
+    new_release = 0 \
+            if args.version.is_devrelease \
+            or args.version.is_prerelease \
+            or args.version.is_postrelease \
+            else 1
     if env_file := os.getenv('GITHUB_ENV'):
         with open(env_file, "a", encoding='utf-8') as myfile:
             myfile.write("PUBLISH_DOCKER_IMAGE_VALID=1\n")
-            myfile.write(f"PUBLISH_DOCKER_IMAGE={1 if new_release else 0}\n")
+            myfile.write(f"PUBLISH_DOCKER_IMAGE={new_release}\n")
     else:
         sys.stdout.write("PUBLISH_DOCKER_IMAGE_VALID=1\n")
-        sys.stdout.write(f"PUBLISH_DOCKER_IMAGE={1 if new_release else 0}\n")
+        sys.stdout.write(f"PUBLISH_DOCKER_IMAGE={new_release}\n")
+
