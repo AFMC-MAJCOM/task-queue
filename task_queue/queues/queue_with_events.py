d7f7682cdea2aeb799cb82d71c16a953cef5139f
AM-USAF <anna.mendenhall.1@us.af.mil>
2024-09-24 12:58:31 -0600

    OPAL-1034 Drop Pytest SQL tables (#90)
    * allow renaming of sql table and drop after pytest

--- task_queue/queues/queue_with_events.py
+++ task_queue/queues/queue_with_events.py
@@ -65,6 +65,7 @@ class QueueWithEvents(QueueBase):
     """
     # Pylint does not like more than 5 parameters
     # pylint: disable=too-many-arguments
+    # pylint: disable=too-many-positional-arguments
     def __init__(
         self,
         queue,
